0ce49a551d64072e1235a20cd67a75f9b93216b2
Kevin <kevinleitesolar@hotmail.com>
2026-04-04 19:38:48 -0300

    first commit. project only with the frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import { PeriodSection } from "@/components/period-section/period-section"
+import { groupAppointmentByPeriod, APPOINTMENTS } from "@/utils"
+
+export default function Home() {
+  const periods = groupAppointmentByPeriod(APPOINTMENTS)
+
+  return (
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title-size text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium-size text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
+
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
