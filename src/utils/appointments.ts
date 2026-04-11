8649d9dfa0534ccfa1eee67aaf559f69be7410ca
Kevin WSL <kevinleitesolar@hotmail.com>
2026-04-11 16:48:36 -0300

    chore: edit script to run prisma migrations and build the project after that

--- src/utils/appointments.ts
+++ src/utils/appointments.ts
@@ -48,7 +48,7 @@ export function groupAppointmentByPeriod(
       appointments: afternoonAppointments,
     },
     {
-      title: "Manhã",
+      title: "Noite",
       type: "evening",
       timeRange: "19h-21h",
       appointments: eveningAppointments,
